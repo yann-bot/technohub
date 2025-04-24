829c8bfa6714eeecac749ab20a673dadda0c080c
YannOuafete <yannouafete@gmail.com>
2025-04-24 18:51:47 +0100

    coding home page

--- technohub/src/app/page.tsx
+++ technohub/src/app/page.tsx
@@ -1,103 +1,74 @@
-import Image from "next/image";
+
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const items = [
+        'item1',
+        'item2',
+        'item3',
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    ]
+    const elements = [];
+
+    for (let i = 0; i < items.length; i++) {
+        elements.push(<div key={i}>{items[i]}</div>);
+    }
+
+   return(
+       <div>
+            <header className='border '>
+                 <nav>
+                     <div>TechnoHub</div>
+                     <ul>
+                       <li>Devenir Technicien</li>
+                       <li>Se connecter</li>
+                     </ul>
+                 </nav>
+                 <div>
+                    <div>
+                        <h1>Trouver un technicien qualifié</h1>
+                        <p>Des milliers de techniciens sont prêts à répondre à vos besoins</p>
+                    </div>
+                     <div>
+                        <input type='search' />
+                        <button>Search</button>
+                     </div>
+                     <div>
+                         {items.map((fruit, index) => (
+                             <div key={index}>{fruit}</div>
+                         ))}
+                     </div>
+                 </div>
+            </header>
+
+             <main className='border border-red-500 mt-1'>
+                 <section>
+                    {/* Liste des techniciens afficher de façon aléatoire  */}
+                     <h2>Techniciens qualifiés à votre écoute</h2>
+                 </section>
+                 <section>
+                     <div>Comment ça marche</div>
+                 </section>
+                 <section>
+                     {/* Devenir technicien */}
+                 </section>
+
+             </main>
+
+             <footer className='border border-blue-500 mt-1'>
+                 <div>
+                     <section></section>
+                     <section></section>
+                     <section></section>
+                     <section></section>
+                 </div>
+                 <div>
+
+                 </div>
+
+             </footer>
+
+       </div>
+   )
 }
