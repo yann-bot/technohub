e88a36f9bbfda9aab339b358205590d5a3b203f0
YannOuafete <yannouafete@gmail.com>
2025-04-29 07:12:20 +0100

    name change

--- nzonizo/src/app/components/footer.tsx
+++ nzonizo/src/app/components/footer.tsx
@@ -28,11 +28,10 @@ export default function Footer() {
                     </ul>
                 </section>
                 <section className={clsx('md:flex md:flex-col gap-5' )}>
-                    <h1 className={clsx("text-2xl font-sans tracking-widest")}>A propos</h1>
+                    <h1 className={clsx("text-2xl font-sans tracking-widest")}>Assistance</h1>
                     <ul className={clsx('md:flex md:flex-col gap-2 text-sm font-serif tracking-widest')}>
-                        <li><Link href="#">Qui sommes-nous</Link></li>
-                        <li><Link href="#">Mentions légales</Link></li>
-                        <li><Link href="#">Confidentialité</Link></li>
+                        <li><Link href="#">Centre d'aide</Link></li>
+                        <li><Link href="#">Contact</Link></li>
                     </ul>
                 </section>
 
@@ -46,7 +45,7 @@ export default function Footer() {
                 </section>
             </div>
             <div className={clsx(' border-white border-t-1  font-serif text-sm tracking-wider pt-2 ')}>
-                 2025,Global Consortium
+                © 2025,NzoniZo
             </div>
 
         </footer>
